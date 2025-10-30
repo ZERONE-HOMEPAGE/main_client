ecc05fb5767393dd5b2cb88f909b60134f983d8d
hanbi06 <sjo480524@gmail.com>
2025-10-31 03:37:53 +0900

    (#64) 배경 및 텍스트 작업 + 그외
    최대한 디자인안과 유사하게 배경과 히어로 텍스트를 삽입하였습니다.
    
    박스 크기를 고정시켰습니다.
    
    + 문답 박스를 여러개 활성화 할 시 창 크기가 너무 길어지므로 전체중 하나만 활성화 될 수 있도록 작업할 예정입니다.

--- src/components/sections/QnAPage/QnABox.tsx
+++ src/components/sections/QnAPage/QnABox.tsx
@@ -12,8 +12,8 @@ export default function QnA({ Q, A } : QnAProps ) {
   if (A === "") A = "야호"; // 테스트
 
   return (
-    <div onClick={() => setOpen((prev) => !prev)} className="m-2 bg-white shadow-lg flex flex-col justify-center border border-[#E5E5EC] rounded-lg px-8 py-5 cursor-pointer transition-shadow duration-300 hover:shadow-xl hover:bg-[#F4F0FF]">
-      <div className="flex w-full justify-between items-center mb-4">
+    <div onClick={() => setOpen((prev) => !prev)} className="w-full m-2 bg-white shadow-lg flex flex-col justify-center border border-[#E5E5EC] rounded-lg px-8 py-5 cursor-pointer transition-shadow duration-300 hover:shadow-xl hover:bg-[#F4F0FF]">
+      <div className="flex w-full justify-between items-center flex-1 break-keep">
         <p className="text-lg font-semibold">{Q}</p>
         <img
           src={DownIcon}
@@ -23,16 +23,16 @@ export default function QnA({ Q, A } : QnAProps ) {
           }`}
         />
       </div>
+
       <div
-        className={`grid transition-[grid-template-rows,gap] duration-300 ease-in-out ${
+        className={`grid transition-[grid-template-rows,gap] duration-100 ease-linear ${
           open ? "grid-rows-[auto_1fr] gap-4" : "grid-rows-[auto_0fr] gap-0"
-        }`}
-      >
-        <div className="overflow-hidden transition-opacity duration-300 text-gray-700">
+      }`}>
+        <div className="overflow-hidden transition-opacity duration-700 ease-linear">
           {open && (
             <>
-              <hr className="w-full border-t border-[#E5E5EC] mb-4" />
-              <p className="leading-relaxed">{A}</p>
+              <hr className="w-full border-t border-[#E5E5EC] my-4" />
+              <p className="leading-relaxed text-base" dangerouslySetInnerHTML={{ __html: A }}></p>
             </>
           )}
         </div>
--- src/pages/QnAPage.tsx
+++ src/pages/QnAPage.tsx
@@ -6,7 +6,7 @@ export default function QnAPage() {
   const qna = [
     {
       Question : "스터디는 어떻게 진행되나요?",
-      Answer : "영과일에서는 학습 수준에 따라 C기초반, 브릿지반, 알고리즘반 등 다양한 단계의 스터디를 운영하고 있습니다. 각 스터디는 경험 많은 멘토들이 직접 지도하며, 자신의 실력에 맞게 배우고 성장할 수 있는 환경을 제공합니다.()"
+      Answer : "영과일에서는 학습 수준에 따라 C기초반, 브릿지반, 알고리즘반 등 다양한 단계의 스터디를 운영하고 있습니다.<br/>각 스터디는 경험 많은 멘토들이 직접 지도하며, 자신의 실력에 맞게 배우고 성장할 수 있는 환경을 제공합니다.()"
     },
     {
 
@@ -23,19 +23,24 @@ export default function QnAPage() {
     },
     {
       Question : "활동 시간이 어떻게 되나요?",
-      Answer : "정규 스터디는 주 1~2회 진행되며, 시간은 팀별로 조율하여 결정합니다. 오프라인/온라인 병행 운영으로 일정이 바쁜 경우에도 무리 없이 참여할 수 있습니다.()"
+      Answer : "정규 스터디는 주 1~2회 진행되며, 시간은 팀별로 조율하여 결정합니다.<br/> 오프라인/온라인 병행 운영으로 일정이 바쁜 경우에도 무리 없이 참여할 수 있습니다.()"
     },
     {
       Question : "회비는 얼마인가요?",
-      Answer : "영과일은 회비 없이 운영되고 있으며, 일부 오프라인 행사 또는 대회 참여 시 필요에 따라 소액의 비용이 발생할 수 있습니다. 이 경우 사전에 충분히 공지해드립니다.()"
+      Answer : "영과일은 회비 없이 운영되고 있으며, 일부 오프라인 행사 또는 대회 참여 시 필요에 따라 소액의 비용이 발생할 수 있습니다.<br/> 이 경우 사전에 충분히 공지해드립니다.()"
     },
   ];
 
   return (
-    <div className="flex h-full w-full flex-col px-20">
-      <div className="flex w-full justify-between items-center">
+    <div className="relative isolate min-h-screen flex h-full w-full flex-col px-20 overflow-hidden">
+      <div className="absolute left-[-12%] top-[22%] w-[700px] h-[700px] rounded-full
+                      bg-[#B5A9FF] opacity-40 blur-[140px] z-0" />
+      <div className="absolute left-[20%] top-[6%] w-[500px] h-[500px] rounded-full
+                      bg-[#60A5FA] opacity-40 blur-[150px] z-0" />
+
+      <div className="relative z-10 flex w-full justify-between items-center mt-40 mb-20">
         <QnAIntro />
-        <div className="flex flex-col">
+        <div className="flex flex-col w-1/2">
           {qna.map((QA, key) => (
             <QnABox 
               key={key}
