e82d58abce6930e16bd01cc81e31e4f4cde19afb
mokmokahr <109899411+mokmokahr@users.noreply.github.com>
2026-02-09 21:46:17 +0900

    feat: Add api layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,22 @@
 // import { StrictMode } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(<App />);
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
+
+createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+    <ReactQueryDevtools buttonPosition="bottom-right" />
+  </QueryClientProvider>,
+);
